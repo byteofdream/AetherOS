6ff64a4765b74d2a7dafacf9b444bad7842b9c54
DreamyByte Labs <dozirod@mail.ru>
2026-03-31 19:40:43 +0300

    Add files via upload

---
+++ userland/include/elf.h
@@ -0,0 +1,8 @@
+#ifndef USERLAND_ELF_H
+#define USERLAND_ELF_H
+
+#include "common.h"
+
+bool elf_run(const void *image, size_t size);
+
+#endif
---
+++ userland/include/shell.h
@@ -0,0 +1,6 @@
+#ifndef USERLAND_SHELL_H
+#define USERLAND_SHELL_H
+
+void shell_task(void *arg);
+
+#endif
