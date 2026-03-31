0b78a1c4bb6b436e0d2517a437af11c9f17fbce8
DreamyByte Labs <dozirod@mail.ru>
2026-03-31 19:33:10 +0300

    Add ATA header file with function declarations

---
+++ drivers/include/ata.h
@@ -0,0 +1,10 @@
+#ifndef DRIVERS_ATA_H
+#define DRIVERS_ATA_H
+
+#include "common.h"
+
+bool ata_init(void);
+bool ata_read28(uint32_t lba, uint8_t sector_count, void *buffer);
+bool ata_write28(uint32_t lba, uint8_t sector_count, const void *buffer);
+
+#endif
